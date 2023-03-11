def2344a587dd9baeafbd91138a3806d215e1b9f
tiago <tiagomateus.fonte13@ua.pt>
2023-03-11 16:36:51 +0000

    updated

--- part3/part3.py
+++ part3/part3.py
@@ -27,7 +27,7 @@ inputs = [1, 2, 3, 2.5]
 weights = [0.2, 0.8, -0.5, 1.0]
 bias = 2
 
-output = np.dot(inputs, weights) + bias
+output = np.dot(weights, inputs) + bias
 print(output)
 
 # ex3, dot product demonstration for three neurons using NumPy-------------------------------------
@@ -40,5 +40,5 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
 
-output = np.dot(inputs, weights) + bias
+output = np.dot(weights, inputs) + biases
 print(output)
